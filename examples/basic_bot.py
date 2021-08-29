cb19803f0248b95bc64fd906df5c7aaf90cc2300
Leon Bowie <leon@bowie-co.nz>
2021-08-30 02:45:21 +1200

    add basic bot example

---
+++ examples/basic_bot.py
@@ -0,0 +1,20 @@
+import defectio
+
+client = defectio.Client()
+
+
+@client.event
+async def on_ready():
+    print("We have logged in.")
+
+
+@client.event
+async def on_message(message: defectio.Message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+client.run("bot_token")
