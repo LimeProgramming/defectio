8f64dc07e93a16795102a158aefebe9aaf574bfb
Leon Bowie <leon@bowie-co.nz>
2021-09-17 14:46:29 +1200

    version 0.2.0

--- defectio/__init__.py
+++ defectio/__init__.py
@@ -13,7 +13,7 @@ __title__ = "defectio"
 __author__ = "Darkflame72"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present Darkflame72"
-__version__ = "0.1.11a"
+__version__ = "0.2.0a"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
@@ -78,7 +78,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=0, minor=1, micro=11, releaselevel="alpha", serial=0
+    major=0, minor=2, micro=0, releaselevel="alpha", serial=0
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
