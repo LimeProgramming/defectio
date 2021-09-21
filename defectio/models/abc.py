0327381c7881252f7f9ea660743bfcd107c14d5b
Adam <LimeProgramming64@gmail.com>
2021-09-21 23:32:15 +0100

    hate you github

--- defectio/models/abc.py
+++ defectio/models/abc.py
@@ -1,33 +1,56 @@
 from __future__ import annotations
+from defectio.types.payloads import ChannelPayload
+from defectio.models.server import Category
 
-import asyncio
-import copy
-import sys
 from typing import Any
-from typing import Dict
 from typing import Optional
 from typing import Protocol
 from typing import runtime_checkable
 from typing import TYPE_CHECKING
-from ..errors import InvalidArgument
-from .message import(
-    File,
-    AutumnID,
-
-)
+from typing import Union
 
 if TYPE_CHECKING:
     from ..state import ConnectionState
     from .server import Server
+    from .message import Message, Reply
+    from ..types.payloads import ChannelType
+    from .channel import DMChannel, TextChannel, GroupChannel
+    from defectio.models.message import File
+    from .user import ClientUser
+
+    PartialMessageableChannel = Union[TextChannel, DMChannel]
+    MessageableChannel = Union[PartialMessageableChannel, GroupChannel]
+
+
+class DefectioBase(Protocol):
+    """An ABC that details the common operations on a Defectio model.
+
+    Almost all :ref:`Defectio models <defectoi_api_models>` meet this
+    abstract base class.
+
+    If you want to create a defectiobase on your own, consider using
+    :class:`.Object`.
+
+    Attributes
+    -----------
+    id: :class:`int`
+        The model's unique ID.
+    """
+
+    __slots__ = ()
+    id: int
 
 
 @runtime_checkable
-class User(Protocol):
+class User(DefectioBase, Protocol):
     """An ABC that details the common operations on a Revolt user.
+
     The following implement this ABC:
+
     - :class:`~defectio.User`
-    - :class:`~defectio.ClientUser`
+    - :class:`~discord.ClientUser`
     - :class:`~defectio.Member`
+
     Attributes
     -----------
     name: :class:`str`
@@ -52,6 +75,28 @@ class User(Protocol):
         raise NotImplementedError
 
 
+@runtime_checkable
+class PrivateChannel(DefectioBase, Protocol):
+    """An ABC that details the common operations on a private Discord channel.
+
+    The following implement this ABC:
+
+    - :class:`~defectio.DMChannel`
+    - :class:`~defectio.GroupChannel`
+
+    This ABC must also implement :class:`~defectio.abc.DefectioBase`.
+
+    Attributes
+    -----------
+    me: :class:`~discord.User`
+        The user presenting yourself.
+    """
+
+    __slots__ = ()
+
+    me: ClientUser
+
+
 class ServerChannel:
     __slots__ = ()
 
@@ -60,105 +105,93 @@ class ServerChannel:
     server: Server
     type: str
     position: int
-    category_id: Optional[int]
     _state: ConnectionState
 
     if TYPE_CHECKING:
 
         def __init__(
-            self, *, state: ConnectionState, server: Server, data: Dict[str, Any]
+            self, *, state: ConnectionState, server: Server, data: ChannelPayload
         ):
             ...
 
     def __str__(self) -> str:
         return self.name
 
-    def _update(self, server: Server, data: Dict[str, Any]) -> None:
+    def _update(self, server: Server, data: dict[str, Any]) -> None:
         raise NotImplementedError
 
-    async def delete(self, *, reason: Optional[str] = None) -> None:
+    @property
+    def mention(self) -> str:
+        return f"<#{self.id}>"
+
+    async def delete(self) -> None:
         await self._state.http.close_channel(self.id)
+
+    @property
+    def category(self) -> Optional[Category]:
+        """Optional[:class:`~defectio.Category`]: The category this channel belongs to.
+
+        If there is no category then this is ``None``.
+        """
+        return self.server.get_category_channel(self.id)
 
 
 class Messageable(Protocol):
+    """An ABC that details the common operations on a model that can send messages.
+
+    The following implement this ABC:
+
+    - :class:`~defectio.TextChannel`
+    - :class:`~defectio.DMChannel`
+    - :class:`~defectio.GroupChannel`
+    - :class:`~defectio.User`
+    - :class:`~defectio.Member`
+    """
 
     __slots__ = ()
     _state: ConnectionState
 
-    async def _get_channel(self):
+    async def _get_channel(self) -> MessageableChannel:
         raise NotImplementedError
 
     async def send(
         self,
         content: str = None,
         *,
-        file=None,
-        files=None,
+        file: Optional[File] = None,
+        files: Optional[list[File]] = None,
+        replies: Optional[list[Reply]] = [],
         delete_after: int = None,
         nonce=None,
     ):
-        state = self._state
-        attachments = []
+
         channel = await self._get_channel()
-        content = str(content) if content is not None else None
+        state = self._state
+        content = str(content) if content is not None else ""
 
-        # if mention_author is not None:
-        #     allowed_mentions = allowed_mentions or AllowedMentions().to_dict()
-        #     allowed_mentions["replied_user"] = bool(mention_author)
-
-        # if reference is not None:
-        #     try:
-        #         reference = reference.to_message_reference_dict()
-        #     except AttributeError:
-        #         raise InvalidArgument(
-        #             "reference parameter must be Message or MessageReference"
-        #         ) from None
-
-        if file is not None and files is not None:
-            raise InvalidArgument("cannot pass both file and files parameter to send()")
-
+        attachment_ids: list[str] = []
         if file is not None:
-            if not isinstance(file, File):
-                raise InvalidArgument("file parameter must be File")
-
-            try:
-                data = await state.http.send_file(
-                    file= file,
-                    tag = "attachments"
+            files = [file]
+        if files is not None:
+            for attachment in files:
+                attach = await self._state.http.send_file(
+                    file=attachment, tag="attachments"
                 )
-                attachments = [AutumnID(data=data).id]
-
-            finally:
-                file.close()
-
-        elif files is not None:
-            if len(files) > 4:
-                raise InvalidArgument("files parameter must be a list of up to 4 elements")
-
-            elif not all(isinstance(file, File) for file in files):
-                raise InvalidArgument("files parameter must be a list of File")
-
-            try:
-                data = await state.http.send_file(
-                    file= file,
-                    tag = "attachments"
-                )
-                attachments.append(AutumnID(data=data).id)
-
-            finally:
-                for f in files:
-                    f.close()
-
+                attachment_ids.append(attach["id"])
+        
+        replies = [{"id": r.message.id, "mention": r.mention} for r in replies]
         data = await state.http.send_message(
             channel.id,
-            content,
-            attachments = attachments
+            content=content,
+            attachments=attachment_ids,
+            replies=replies
         )
 
-        ret = state.create_message(channel=channel, data=data)
+
+        new_message = state.create_message(channel=channel, data=data)
         if delete_after is not None:
-            await ret.delete(delay=delete_after)
-        return ret
+            await new_message.delete(delay=delete_after)
+        return new_message
 
     async def fetch_message(self, id):
         channel = await self._get_channel()
@@ -172,3 +205,32 @@ class Messageable(Protocol):
     async def stop_typing(self):
         channel = await self._get_channel()
         await self._state.websocket.stop_typing(channel.id)
+
+    async def fetch_message(self, id: int) -> Message:
+        """|coro|
+
+        Retrieves a single :class:`~defectio.Message` from the destination.
+
+        Parameters
+        ------------
+        id: :class:`int`
+            The message ID to look for.
+
+        Raises
+        --------
+        ~defectio.NotFound
+            The specified message was not found.
+        ~defectio.Forbidden
+            You do not have the permissions required to get a message.
+        ~defectio.HTTPException
+            Retrieving the message failed.
+
+        Returns
+        --------
+        :class:`~defectio.Message`
+            The message asked for.
+        """
+
+        channel = await self._get_channel()
+        data = await self._state.http.get_message(channel.id, id)
+        return self._state.create_message(channel=channel, data=data)
