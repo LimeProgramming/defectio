9c4a9eb99b68f82a151813cc93ecd7ad52012935
Adam <LimeProgramming64@gmail.com>
2021-09-21 23:29:06 +0100

    hate you github

--- defectio/__init__.py
+++ defectio/__init__.py
@@ -13,7 +13,7 @@ __title__ = "defectio"
 __author__ = "Darkflame72"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present Darkflame72"
-__version__ = "0.1.4a"
+__version__ = "0.2.0a"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
@@ -21,7 +21,38 @@ import logging
 from typing import NamedTuple, Literal
 
 from .client import Client
-from .models import Message, User, Server, Member, File, Attachment
+from .models import (
+    Message,
+    Reply,
+    User,
+    Server,
+    Member,
+    Role,
+    RawMessageDeleteEvent,
+    RawMessageUpdateEvent,
+    TextChannel,
+    VoiceChannel,
+    GroupChannel,
+    DMChannel,
+    Category,
+    Status,
+    File,
+    Attachment,
+)
+from .models import abc
+from . import utils
+from .errors import (
+    DefectioException,
+    ClientException,
+    GatewayNotFound,
+    HTTPException,
+    Forbidden,
+    RevoltServerError,
+    InvalidArgument,
+    InvalidData,
+    LoginFailure,
+)
+
 
 __all__ = (
     "__title__",
@@ -34,8 +65,7 @@ __all__ = (
     "User",
     "Server",
     "Member",
-    "File",
-    "Attachment"
+    "abc",
 )
 
 
@@ -48,7 +78,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=0, minor=1, micro=4, releaselevel="alpha", serial=0
+    major=0, minor=2, micro=0, releaselevel="alpha", serial=0
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
